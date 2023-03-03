e49c0d948fe9e422d4e3cbeeaadff081a2275cfe
= <=>
2023-03-03 16:53:42 -0300

    Segundo comit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  data(){
+    return { 
+      contador: 10
+  }
+  },
+  methods:{
+    incrementar(){
+      this.contador++
+    },
+    decrementar (){
+      if (this.contador > 0) {
+        this.contador--
+      }
+    },
+    reset(){
+      this.contador = 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="incrementar">+</button>
+  <button @click="decrementar">-</button>
+  <button @click="reset">c</button>
+  <p>Valor do contador: {{  contador }}</p>
+</template>
+
+<style scoped>
+button {
+  margin-left: 10px;
+  background-color: brown;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;  
+}</style>
